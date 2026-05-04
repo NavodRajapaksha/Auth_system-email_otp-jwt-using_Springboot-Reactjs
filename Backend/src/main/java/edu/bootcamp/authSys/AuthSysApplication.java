c72e825f94345da489f2b2e35c6b52e890dada11
Navod D Rajapaksha <129612778+NavodRajapaksha@users.noreply.github.com>
2026-05-05 00:19:51 +0530

    chore: initial project setup with frontend and backend structure

---
+++ Backend/src/main/java/edu/bootcamp/authSys/AuthSysApplication.java
@@ -0,0 +1,13 @@
+package edu.bootcamp.authSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSysApplication.class, args);
+	}
+
+}
