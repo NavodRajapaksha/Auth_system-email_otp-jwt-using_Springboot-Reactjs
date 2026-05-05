8b3e94b09a6f36cf0d87b38f8861fe1a920023d9
Navod D Rajapaksha <129612778+NavodRajapaksha@users.noreply.github.com>
2026-05-05 21:20:18 +0530

    feat : Added user entity

---
+++ Backend/src/main/java/edu/bootcamp/authSys/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package edu.bootcamp.authSys.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Builder
+@Table(name = "tbl_users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String verifyOtp;
+    private Boolean isAccountVerified;
+    private Long verifyOtpExpireAt;
+    private String resetOtp;
+    private Long resetOtpExpireAt;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
