dc0a269399eddacfe64c2ef552cd5a17d754983b
Navod D Rajapaksha <129612778+NavodRajapaksha@users.noreply.github.com>
2026-05-21 21:34:08 +0530

    feat : added cookies and jwttoken

--- Backend/src/main/java/edu/bootcamp/authSys/controller/AuthController.java
+++ Backend/src/main/java/edu/bootcamp/authSys/controller/AuthController.java
@@ -1,20 +1,24 @@
 package edu.bootcamp.authSys.controller;
 
 import edu.bootcamp.authSys.dto.request.AuthRequest;
+import edu.bootcamp.authSys.dto.response.AuthResponse;
 import edu.bootcamp.authSys.service.impl.AppUserDetailsService;
+import edu.bootcamp.authSys.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,22 +28,35 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final AppUserDetailsService appUserDetailsService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest authRequest){
         try{
             authenticate(authRequest.getEmail(), authRequest.getPassword());
             final UserDetails userDetails = appUserDetailsService.loadUserByUsername(authRequest.getEmail());
+            final String jwtToken = jwtUtil.generateToken(userDetails);
+            ResponseCookie responseCookie = ResponseCookie.from("jwt", jwtToken)
+                    .httpOnly(true)
+                    .path("/")
+                    .maxAge(Duration.ofDays(1))
+                    .sameSite("Strict")
+                    .build();
+            return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                    .body(new AuthResponse(authRequest.getEmail(), jwtToken));
+
         } catch (BadCredentialsException ex){
             Map<String, Object> error = new HashMap<>();
             error.put("error", true);
             error.put("message", "Email or password is incorrect");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+
         } catch (DisabledException ex) {
             Map<String, Object> error = new HashMap<>();
             error.put("error", true);
             error.put("message", "Account is Disabled");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+
         } catch (Exception ex) {
             Map<String, Object> error = new HashMap<>();
             error.put("error", true);
